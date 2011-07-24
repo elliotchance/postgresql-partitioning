4c0ad9e46482f68b34c8eed5bf5e837dc33f56b4
Elliot Chance <elliotchance@me.com>
2011-07-24 14:14:10 +1000

    Added partitioning by range. Closes #2.

--- install.sql
+++ install.sql
@@ -19,14 +19,15 @@ BEGIN
 		table_name TEXT NOT NULL REFERENCES _partition_table(table_name)
 			ON DELETE CASCADE ON UPDATE CASCADE,
 		partition_name TEXT NOT NULL,
-		test TEXT NOT NULL,
+		test_lower TEXT NOT NULL,
+		test_upper TEXT,
 		PRIMARY KEY ( table_name, partition_name )
 	);
 	
 	-- views
 	CREATE VIEW pg_partition AS
 	SELECT _partition.table_name, _partition.partition_name, _partition_table.partition_type,
-		expression, test
+		expression, test_lower, test_upper
 	FROM _partition, _partition_table
 	WHERE _partition.table_name=_partition_table.table_name;
 END;
@@ -67,19 +68,32 @@ CREATE OR REPLACE FUNCTION _add_partition(
 	the_table_name TEXT,
 	the_partition_name TEXT,
 	the_expression TEXT,
-	the_list_value TEXT
+	the_min_value TEXT,
+	the_max_value TEXT
 )
 RETURNS BOOLEAN
 AS $$
+DECLARE
+	compare_type TEXT;
 BEGIN
+	-- get compare type
+	compare_type := _get_comparator_type(the_table_name);
+
 	-- create partition table
-	EXECUTE 'CREATE TABLE ' || the_table_name || '_' || the_partition_name || '( CHECK((' ||
-		the_expression || ')::text = (''' || the_list_value || ''')::text) ) INHERITS (' ||
-		the_table_name || ')';
+	IF(the_max_value IS NULL) THEN
+		EXECUTE 'CREATE TABLE ' || the_table_name || '_' || the_partition_name || '( CHECK((' ||
+			the_expression || ')::' || compare_type || ' = (''' || the_min_value || ''')::' ||
+			compare_type || ') ) INHERITS (' || the_table_name || ')';
+	ELSE
+		EXECUTE 'CREATE TABLE ' || the_table_name || '_' || the_partition_name || '( CHECK((' ||
+			the_expression || ')::' || compare_type || ' BETWEEN (''' || the_min_value || ''')::' ||
+			compare_type || ' AND (''' || the_max_value || ''')::' || compare_type ||
+			') ) INHERITS (' || the_table_name || ')';
+	END IF;
 		
 	-- register partition
-	INSERT INTO _partition (table_name, partition_name, test)
-	VALUES (the_table_name, the_partition_name, the_list_value);
+	INSERT INTO _partition (table_name, partition_name, test_lower, test_upper)
+	VALUES (the_table_name, the_partition_name, the_min_value, the_max_value);
 	
 	-- success
 	RETURN TRUE;
@@ -97,19 +111,34 @@ DECLARE
 	part_row RECORD;
 	counter INT;
 	the_trigger_name TEXT;
+	compare_type TEXT;
 BEGIN
+	-- get comparator
+	compare_type := _get_comparator_type(the_table_name);
+
 	-- build trigger function
 	the_trigger := 'CREATE OR REPLACE FUNCTION ' || the_table_name || '_insert_trigger() ' ||
 		'RETURNS TRIGGER AS $TRIGGER$ BEGIN IF(FALSE) THEN /* do nothing */ ';
-		
+	
 	FOR part_row IN SELECT * FROM pg_partition WHERE table_name=the_table_name
 	LOOP
-		the_trigger := the_trigger || ' ELSIF (NEW.' || part_row.expression || '::text = ''' ||
-			part_row.test || '''::text) THEN INSERT INTO ' || the_table_name || '_' ||
-			part_row.partition_name || ' VALUES (NEW.*); ';
+		IF(part_row.test_upper IS NULL) THEN
+			the_trigger := the_trigger || ' ELSIF (NEW.' || part_row.expression || '::' ||
+				compare_type || ' = ''' || part_row.test_lower || '''::' || compare_type ||
+				') THEN INSERT INTO ' || the_table_name || '_' || part_row.partition_name ||
+				' VALUES (NEW.*); ';
+		ELSE
+			the_trigger := the_trigger || ' ELSIF (NEW.' || part_row.expression || '::' ||
+				compare_type || ' BETWEEN ''' || part_row.test_lower || '''::' || compare_type ||
+				' AND ''' || part_row.test_upper || '''::' || compare_type ||
+				') THEN INSERT INTO ' || the_table_name || '_' || part_row.partition_name ||
+				' VALUES (NEW.*); ';
+		END IF;
 	END LOOP;
 	
-	the_trigger := the_trigger || 'ELSE RAISE EXCEPTION ''List value out of range.''; END IF;' ||
+	the_trigger := the_trigger || 'ELSE RAISE EXCEPTION ''Value provided for ' ||
+		(SELECT partition_type FROM _partition_table WHERE table_name=the_table_name) ||
+		' partition out of range.''; END IF;' ||
 		' RETURN NULL; END; $TRIGGER$ LANGUAGE plpgsql';
 	EXECUTE the_trigger;
 	
@@ -143,6 +172,68 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION _partition_table_init(
+	the_table_name TEXT
+)
+RETURNS VOID
+AS $$
+DECLARE
+	counter INT;
+BEGIN
+	-- make sure partitioning feature is ready
+	PERFORM _partition_init();
+
+	-- make sure the table exists
+	IF(NOT _table_exists(the_table_name)) THEN
+		RAISE EXCEPTION 'Table % does not exist.', the_table_name;
+	END IF;
+	
+	-- make sure the table has not been partitioned already
+	IF(_table_is_partitioned(the_table_name)) THEN
+		RAISE EXCEPTION 'Table % is already partitioned.', the_table_name;
+	END IF;
+	
+	-- we can only partition a table that is empty
+	EXECUTE 'SELECT COUNT(*) FROM ' || the_table_name INTO counter;
+	IF(counter > 0) THEN
+		RAISE EXCEPTION 'Only empty tables can be partitioned.', the_table_name;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION _table_exists(
+	the_table_name TEXT
+)
+RETURNS BOOLEAN
+AS $$
+DECLARE
+	counter INT;
+BEGIN
+	SELECT COUNT(*) INTO counter FROM pg_class WHERE relname=the_table_name;
+	RETURN (counter > 0);
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION _get_comparator_type(
+	the_table_name TEXT
+)
+RETURNS TEXT
+AS $$
+BEGIN
+	RETURN (
+		SELECT typname::TEXT
+		FROM pg_attribute
+		JOIN pg_class ON attrelid=pg_class.oid
+		JOIN pg_type ON atttypid=pg_type.oid
+		JOIN _partition_table ON _partition_table.table_name=pg_class.relname
+		WHERE pg_class.relname=the_table_name AND attname=_partition_table.expression
+	);
+END;
+$$ LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION create_table_partition_by_list(
 	the_table_name TEXT,
 	the_expression TEXT,
@@ -155,21 +246,8 @@ DECLARE
 	the_list_value TEXT;
 	the_partition_name TEXT;
 BEGIN
-	-- make sure partitioning feature is ready
-	PERFORM _partition_init();
-
-	-- TODO: make sure the table exists
-	
-	-- make sure the table has not been partitioned already
-	IF(_table_is_partitioned(the_table_name)) THEN
-		RAISE EXCEPTION 'Table % is already partitioned.', the_table_name;
-	END IF;
-	
-	-- we can only partition a table that is empty
-	EXECUTE 'SELECT COUNT(*) FROM ' || the_table_name INTO counter;
-	IF(counter > 0) THEN
-		RAISE EXCEPTION 'Only empty tables can be partitioned.', the_table_name;
-	END IF;
+	-- check some prerequisites
+	PERFORM _partition_table_init(the_table_name);
 	
 	-- register table for partitioning
 	INSERT INTO _partition_table (table_name, partition_type, expression)
@@ -179,7 +257,8 @@ BEGIN
 		SELECT the_list_values[i] FROM generate_series(1, array_upper(the_list_values, 1)) AS i
 	LOOP
 		the_partition_name := substring(md5(random()::text) for 8);
-		PERFORM _add_partition(the_table_name, the_partition_name, the_expression, the_list_value);
+		PERFORM _add_partition(the_table_name, the_partition_name, the_expression, the_list_value,
+			NULL);
 	END LOOP;
 	
 	-- build and attach triggers
@@ -292,7 +371,7 @@ BEGIN
 		des := des || substring('           ' for 11 - length(part.reltuples::text)) ||
 			part.reltuples;
 		des := des || ' | ';
-		des := des || part.expression || '=' || part.test;
+		des := des || part.expression || '=' || part.test_lower;
 		des := des || E'\n';
 	END LOOP;
 	
@@ -334,14 +413,103 @@ BEGIN
 	
 	-- make sure this list value does not exist
 	SELECT count(*) INTO counter FROM _partition WHERE table_name=the_table_name AND
-		test=the_value;
+		test_lower=the_value;
 	IF(counter > 0) THEN
 		RAISE EXCEPTION 'Value % already exists in list partition.', the_value;
 	END IF;
 	
 	-- create partition
 	SELECT expression INTO the_expression FROM _partition_table WHERE table_name=the_table_name;
-	PERFORM _add_partition(the_table_name, the_partition_name, the_expression, the_value);
+	PERFORM _add_partition(the_table_name, the_partition_name, the_expression, the_value, NULL);
+	
+	-- rebuild triggers
+	PERFORM _rebuild_triggers(the_table_name);
+	
+	-- success
+	RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION create_table_partition_by_range(
+	the_table_name TEXT,
+	the_expression TEXT
+)
+RETURNS BOOLEAN
+AS $$
+DECLARE
+	counter INT;
+	the_partition_name TEXT;
+BEGIN
+	-- check some prerequisites
+	PERFORM _partition_table_init(the_table_name);
+	
+	-- register table for partitioning
+	INSERT INTO _partition_table (table_name, partition_type, expression)
+	VALUES (the_table_name, 'range', the_expression);
+	
+	-- build and attach triggers
+	PERFORM _rebuild_triggers(the_table_name);
+	
+	-- success
+	RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION add_range_partition(
+	the_table_name TEXT,
+	the_partition_name TEXT,
+	the_range_min TEXT,
+	the_range_max TEXT
+)
+RETURNS BOOLEAN
+AS $$
+DECLARE
+	counter INT;
+	the_expression TEXT;
+	temp_row RECORD;
+	compare_type TEXT;
+BEGIN
+	-- make sure partitioning feature is ready
+	PERFORM _partition_init();
+	
+	-- get comparator
+	compare_type := _get_comparator_type(the_table_name);
+	
+	-- make sure the table exists and is partitioned
+	IF(NOT _table_is_partitioned(the_table_name)) THEN
+		RAISE EXCEPTION 'Table % is not partitioned.', the_table_name;
+	END IF;
+	
+	-- make sure this table is RANGE partitioned
+	IF((SELECT partition_type FROM _partition_table WHERE table_name=the_table_name) != 'range')
+	THEN
+		RAISE EXCEPTION 'Table % is not partitioned by range.', the_table_name;
+	END IF;
+	
+	-- make sure the partition does not exist
+	SELECT count(*) INTO counter FROM _partition WHERE table_name=the_table_name AND
+		partition_name=the_partition_name;
+	IF(counter > 0) THEN
+		RAISE EXCEPTION 'Partition % already exists.', the_partition_name;
+	END IF;
+	
+	-- make sure this range value does not conflict exist
+	EXECUTE 'SELECT * FROM _partition WHERE table_name=''' || the_table_name || ''' AND ' ||
+		'((''' || the_range_min || '''::' || compare_type || ' BETWEEN test_lower::' ||
+		compare_type || ' AND test_upper::' || compare_type || ') OR ' || '(''' || the_range_max ||
+		'''::' || compare_type || ' BETWEEN test_lower::' || compare_type || ' AND test_upper::' ||
+		compare_type || ')) LIMIT 1' INTO temp_row;
+	IF(temp_row IS NOT NULL) THEN
+		RAISE EXCEPTION 'Range conflicts with an already defined range %-%.', temp_row.test_lower,
+			temp_row.test_upper;
+	END IF;
+	
+	-- create partition
+	SELECT expression INTO the_expression FROM _partition_table WHERE table_name=the_table_name;
+	PERFORM _add_partition(the_table_name, the_partition_name, the_expression, the_range_min,
+		the_range_max);
 	
 	-- rebuild triggers
 	PERFORM _rebuild_triggers(the_table_name);
--- test.sql
+++ test.sql
@@ -19,6 +19,7 @@ AS $$
 BEGIN
 	RETURN NEXT 'List 1: ' || _partition_test_list1();
 	RETURN NEXT 'List 2: ' || _partition_test_list2();
+	RETURN NEXT 'Range 1: ' || _partition_test_range1();
 	RETURN;
 END;
 $$ LANGUAGE plpgsql;
@@ -46,7 +47,7 @@ BEGIN
 	
 	-- validate
 	EXECUTE 'SELECT COUNT(*) FROM testpart_' ||
-		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test='1')
+		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test_lower='1')
 		INTO counter;
 	PERFORM _partition_assert(1::text, counter::text);
 	
@@ -66,7 +67,7 @@ BEGIN
 	
 	-- validate
 	EXECUTE 'SELECT COUNT(*) FROM testpart_' ||
-		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test='1')
+		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test_lower='1')
 		INTO counter;
 	PERFORM _partition_assert(2::text, counter::text);
 	
@@ -104,8 +105,63 @@ BEGIN
 	
 	-- validate
 	EXECUTE 'SELECT COUNT(*) FROM testpart_' ||
-		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test='c d')
+		(SELECT partition_name FROM pg_partition WHERE table_name='testpart' AND test_lower='c d')
 		INTO counter;
+	PERFORM _partition_assert(1::text, counter::text);
+	
+	-- drop table
+	PERFORM drop_partitioned_table('testpart');
+	
+	-- success
+	RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION _partition_test_range1()
+RETURNS BOOLEAN
+AS $$
+DECLARE
+	counter INT;
+BEGIN
+	-- create a table
+	CREATE TABLE testpart (
+		parter INT,
+		some_value TEXT
+	);
+	
+	-- create partitions
+	PERFORM create_table_partition_by_range('testpart', 'parter');
+	PERFORM add_range_partition('testpart', 'range1', '1', '3');
+	PERFORM add_range_partition('testpart', 'range2', '4', '6');
+	
+	-- insert rows
+	INSERT INTO testpart (parter, some_value) VALUES (2, 'abc');
+	INSERT INTO testpart (parter, some_value) VALUES (5, 'def');
+	INSERT INTO testpart (parter, some_value) VALUES (6, 'xyz');
+	
+	-- validate
+	SELECT COUNT(*) INTO counter FROM testpart_range2;
+	PERFORM _partition_assert(2::text, counter::text);
+	
+	-- add a partition
+	PERFORM add_range_partition('testpart', 'range3', '15', '20');
+	
+	-- insert row into newly created partition
+	INSERT INTO testpart (parter, some_value) VALUES (17, 'qwerty');
+	
+	-- validate
+	SELECT COUNT(*) INTO counter FROM testpart_range3;
+	PERFORM _partition_assert(1::text, counter::text);
+	
+	-- move a row to a different partition
+	UPDATE testpart SET parter=1, some_value='works' WHERE some_value='xyz';
+	
+	-- validate
+	SELECT COUNT(*) INTO counter FROM testpart_range1;
+	PERFORM _partition_assert(2::text, counter::text);
+	
+	SELECT COUNT(*) INTO counter FROM testpart WHERE some_value='works';
 	PERFORM _partition_assert(1::text, counter::text);
 	
 	-- drop table
